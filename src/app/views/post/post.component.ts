e4f4398751ba60036a7cba1556293e4833ce766c
Dmitry Shapran <djmitrofanov@gmail.com>
2024-02-18 16:32:09 +0300

    fix info

--- src/app/views/post/post.component.ts
+++ src/app/views/post/post.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
+  isOpen: boolean = false
   constructor() { }
 
   ngOnInit(): void {
