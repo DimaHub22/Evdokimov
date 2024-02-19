50daf05f702fb727463c1b3806b82308ad864fc2
Dmitry Shapran <djmitrofanov@gmail.com>
2024-02-20 00:23:59 +0300

    add adaptive

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   isOpen: boolean = false
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+
   }
 
 }
