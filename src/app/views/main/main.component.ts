36f1b8eea407ed981601becfee2b18e10a5c1bed
Dmitry Shapran <djmitrofanov@gmail.com>
2024-02-21 15:15:27 +0300

    fix modal close

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {IsOpenService} from "../../shared/service/is-open.service";
 
 @Component({
@@ -6,15 +6,33 @@ import {IsOpenService} from "../../shared/service/is-open.service";
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
-
+export class MainComponent implements OnInit,AfterViewInit {
+  @ViewChild('content') content!:ElementRef
   isOpen: boolean = false
+
   constructor(public serviceOpen:IsOpenService) {
 
   }
+ngAfterViewInit() {
+    // if(document.documentElement.clientHeight <= 430){
+    //   this.content.nativeElement.style.height = '300px'
+    //   console.log(this.content.nativeElement)
+    // }
+
+}
 
   ngOnInit(): void {
 
+  }
+
+  openModal(){
+    this.isOpen = true
+    const height = document.documentElement.clientHeight
+    if(height <= 430){
+      this.content.nativeElement.style.height = '300px'
+    }else{
+      this.content.nativeElement.style.height = '425px'
+    }
   }
 
 
